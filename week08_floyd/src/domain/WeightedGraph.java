5ccd5eeeb9bafbb48d771222827d775205d34e1a
Jonas Van Haeken <jonas.van.haeken@outlook.be>
2021-05-15 23:04:54 +0200

    Week 8 + 9

--- week08_floyd/src/domain/WeightedGraph.java
+++ week08_floyd/src/domain/WeightedGraph.java
@@ -22,25 +22,58 @@ public class WeightedGraph {
     }
 
     public int[][] getPointerMatrix() {
+        int aantal = getAantalKnopen();
         int[][] pointerMatrix = new int[getAantalKnopen()][getAantalKnopen()];
         double[][] DMatrix = this.gewichtenMatrix.clone();
 
+        for (int i = 0; i < DMatrix.length; i++) {
+            DMatrix[i] = DMatrix[i].clone();
+        }
+        for (int k = 0; k < aantal; k++) {
+            for (int i = 0; i < aantal; i++) {
+                for (int j = 0; j < aantal; j++) {
+                    if (DMatrix[i][k] + DMatrix[k][j] < DMatrix[i][j]) {
+                        DMatrix[i][j] = DMatrix[i][k] + DMatrix[k][j];
+                        pointerMatrix[i][j] = k + 1;
+                    }
+                }
+            }
+        }
 
 		return pointerMatrix;
 	}
 
-	public List<Integer> getShortestPath(int i, int j, int[][] pointer) {
+	public List<Integer> getShortestPath(int van, int tot, int[][] pointer) {
 		List<Integer> res = new ArrayList<>();
 
+        if (van == tot) {
+            return res;
+        } else {
+            int via = pointer[van - 1][tot - 1];
+            if (via == 0){
+                res.add(van);
+                res.add(tot);
+            } else {
+                res = getShortestPath(van, via, pointer);
+                res.remove(res.size() - 1); //anders dubbel
+                res.addAll(getShortestPath(via, tot, pointer));
+            }
+        }
 
 		return res;
 
 	}
 
 	public int berekenLengte(List<Integer> pad) {
-		int som = 0;
-	
-		return som;
+        int som = 0;
+        int aantalKnopen = pad.size();
+        int huidigeKnoop, volgendeKnoop;
+        for (int i = 0; i < aantalKnopen - 1; i++) {
+            huidigeKnoop = pad.get(i);
+            volgendeKnoop = pad.get(i + 1);
+            som += this.gewichtenMatrix[huidigeKnoop - 1][volgendeKnoop - 1];
+        }
+        return som;
 	}
 
 }
--- week09_Grafen-Dijkstra/src/domain/Graph.java
+++ week09_Grafen-Dijkstra/src/domain/Graph.java
@@ -3,49 +3,97 @@ package domain;
 import java.util.ArrayList;
 
 public class Graph {
-	private final int[][] gewichtenMatrix;
+    private final int[][] gewichtenMatrix;
     private final int inf = Integer.MAX_VALUE;
 
-	public Graph(int[][] matrix) {
-		if (matrix == null || matrix.length == 0 || matrix.length != matrix[0].length) {
-			throw new IllegalArgumentException();
-		}
+    public Graph(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix.length != matrix[0].length) {
+            throw new IllegalArgumentException();
+        }
 
-		this.gewichtenMatrix = matrix.clone();
-	}
+        this.gewichtenMatrix = matrix.clone();
+    }
 
     private int getAantalKnopen() {
         return gewichtenMatrix.length;
     }
 
 
-	private int[][] initMatrixDijkstra(int vanKnoop) {
-		// laatste rij is rij met kortste lengtes vanuit vanKnoop
-		int[][] res = new int[this.gewichtenMatrix.length + 1][this.gewichtenMatrix.length];
+    private int[][] initMatrixDijkstra(int vanKnoop) {
+        int[][] res = new int[this.gewichtenMatrix.length + 1][this.gewichtenMatrix.length];
+        // laatste rij is rij met kortste lengtes vanuit vanKnoop
+        // oefening 9.3
+        for (int i = 0; i < getAantalKnopen(); i++) {
+            for (int j = 0; j < getAantalKnopen(); j++)
+                res[i][j] = gewichtenMatrix[i][j] != inf ? gewichtenMatrix[i][j] : 0;
+            res[getAantalKnopen()][i] = inf;
+        }
+        for (int i = 0; i <= getAantalKnopen(); i++) {
+            res[i][vanKnoop - 1] = 0;
+        }
+        return res;
+    }
 
-		// oefening 9.3
+    public int[][] Dijkstra(int vanKnoop) {
+        int[][] res = initMatrixDijkstra(vanKnoop);
 
-		return res;
-	}
+        System.out.println("Initiele matrix: \n");
+        printIntMatrix(res);
 
-	public int[][] Dijkstra(int vanKnoop) {
-		int[][] res = initMatrixDijkstra(vanKnoop);
-		
-		System.out.println("Initiele matrix: \n");
-		printIntMatrix(res);
-		
-		// oefening 9.4
+        // oefening 9.4
 
-		return res;
-	}
+        // herhaal voor alle knopen
+        for (int i = 0; i < getAantalKnopen() - 1; i++) {
+            // zoek nieuwe minimale afstand
+            int min = inf;
+            int[] knopenpaar = {inf, inf}; // index die het nieuwe minimum is
+            for (int j = 0; j < getAantalKnopen(); j++) {
+                // herhaal voor alle knopen die al bezocht zijn
+                if (res[getAantalKnopen()][j] != inf) {
+                    for (int k = 0; k < getAantalKnopen(); k++) {
+                        // als knoop k+1 nog niet gevonden is,
+                        // als er een verbinding is tussen knoop j+1 en knoop k+1
+                        // en als de verbinding tussen deze knopen korter is
+                        // dan het minimum tot nog toe
+                        if (res[getAantalKnopen()][k] == inf && res[j][k] != 0 &&
+                                res[getAantalKnopen()][j] + res[j][k] < min) {
+                            // onthoud (index van) dit knopenpaar en hun minimum
+                            knopenpaar[0] = j;
+                            knopenpaar[1] = k;
+                            min = res[getAantalKnopen()][j] + res[j][k];
+                        }
+                    }
+                }
+            }
+            // tussenresultaat wegschrijven indien er verbetering is
+            if (knopenpaar[0] != inf && knopenpaar[1] != inf) {
+                // nieuwe minimum
+                res[getAantalKnopen()][knopenpaar[1]] = min;
+                for (int j = 0; j < getAantalKnopen() - 1; j++) {
+                    // kolom op nul zetten, maar niet op de plaats die het minimum aanlevert
+                    if (j != knopenpaar[0])
+                        res[j][knopenpaar[1]] = 0;
+                }
+            }
+        }
+        return res;
+    }
 
-	private ArrayList<Integer> vindPad(int vanKnoop, int naarKnoop, int[][] res) {
-		ArrayList<Integer> pad = new ArrayList<>();
-		// oefening 9.5
-		return pad;
-	}
+    private ArrayList<Integer> vindPad(int vanKnoop, int naarKnoop, int[][] res) {
+        ArrayList<Integer> pad = new ArrayList<>();
+        // oefening 9.5
+        pad.add(naarKnoop);
+        while (naarKnoop != vanKnoop) {
+            int k = 1;
+            while (k - 1 < getAantalKnopen() && res[k - 1][naarKnoop - 1] == 0)
+                k++;
+            pad.add(0, k);
+            naarKnoop = k;
+        }
+        return pad;
+    }
 
-	public String berekenPaden(int vanKnoop) {
+    public String berekenPaden(int vanKnoop) {
         String uit = "";
         int[][] res = this.Dijkstra(vanKnoop);
 
@@ -67,20 +115,20 @@ public class Graph {
             }
         }
 
-		return uit;
-	}
-	
-	private static void printIntMatrix(int[][] matrix) {
-		String result ="";
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[0].length; j++) {
-				result += (matrix[i][j] == Integer.MAX_VALUE ? "inf" : matrix[i][j]) + "\t";
-			}
-			result += "\n";
-		}
-		result += "\n";
-		
-		System.out.println(result);		
-	}
+        return uit;
+    }
+
+    private static void printIntMatrix(int[][] matrix) {
+        String result = "";
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                result += (matrix[i][j] == Integer.MAX_VALUE ? "inf" : matrix[i][j]) + "\t";
+            }
+            result += "\n";
+        }
+        result += "\n";
+
+        System.out.println(result);
+    }
 
 }
